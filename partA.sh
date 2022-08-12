247002ed691fcabe4a560fe5c02ced9d731c9061
rohandry <rohandrysdale95@gmail.com>
2022-08-12 15:32:11 +1000

    initial commit

--- partA.sh
+++ partA.sh
@@ -4,7 +4,7 @@ mkdir assignmentFiles
 ###### Create a mess of containers and images
 
 echo "Making folders"
-mkdir -p assignmentFiles/docs
+mkdir -p generatedFiles/alansDocs
 echo "Creating dummy image"
 DUMMY=docker.io/bitnami/nginx
 podman pull $DUMMY
@@ -25,11 +25,11 @@ echo "Creating changes image and container"
 podman run --name practiceContainer -d docker.io/redhat/ubi8-minimal sleep 10s
 
 echo "Making changes"
-echo "im so friggen board" > assignmentFiles/docs/hardWork.txt
-podman cp  assignmentFiles/docs/hardWork.txt practiceContainer:/home/
+echo "im so friggen board" > generatedFiles/alansDocs/hardWork.txt
+podman cp  generatedFiles/alansDocs/hardWork.txt practiceContainer:/home/
 #TODO FIRST NOTE TO REDHATTER
-echo "Dear Redhatter, if you're reading this its probably too late.........." > assignmentFiles/docs/toRedHatter.txt
-podman cp  assignmentFiles/docs/toRedHatter.txt practiceContainer:/home/
+echo "Dear Redhatter, if you're reading this its probably too late.........." > generatedFiles/alansDocs/toRedHatter.txt
+podman cp  generatedFiles/alansDocs/toRedHatter.txt practiceContainer:/home/
 podman exec -lit mv /home/toRedHatter.txt /home/.toRedHatter.txt
 for FILE in fixLogs.sh makeAccounts.sh doGroups.sh setupOpenshift.sh ansiblejob.sh ammenderrors.sh deleteOldUsers.sh
 do
@@ -39,15 +39,4 @@ done
 ###### Debug a boken container
 echo "Creating broken Container"
 podman run -d --name mysql -e MYSQL_DATABASE=items -e MYSQL_USER=user1 -e MYSQL_PASSWRD=mypa55 -e MYSQL_ROOT_PASSWORD=r00tpa55 -p 30306:3306 docker.io/bitnami/mysql
-echo "podman run -d --name devsql -e MYSQL_DATABASE=items -e MYSQL_USER=user1 -e MYSQL_PASSWRD=mypa55 -e MYSQL_ROOT_PASSWORD=r00tpa55 -p 30306:3306 docker.io/bitnami/mysql" > assignmentFiles/devSqlLaunch.sh
-
-##### Customize a container
-echo "Creating files for container customisation" 
-mkdir assignmentFiles/files
-mkdir assignmentFiles/files/data 
-for FILE in salesdata.xml userreport.pdf
-do
-	touch assignmentFiles/files/${FILE}
-	touch assignmentFiles/files/data/${FILE}
-done
 

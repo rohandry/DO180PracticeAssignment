26a0e1c175cd83687dae13999255a6ab1bb2d997
rohandry <rohandrysdale95@gmail.com>
2022-08-23 16:01:21 +1000

    Make cleanup script safer and greater

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 rm -Rf generatedFiles
-podman rm -af
+podman rm -f toDeleteTest1 toDeleteTest2 toDeleteTest3 toDeleteTest4 practiceContainer mysql
 podman rmi localhost/testimage1 localhost/testimage2 localhost/testimage3 localhost/testimage4
+oc delete project test-project 

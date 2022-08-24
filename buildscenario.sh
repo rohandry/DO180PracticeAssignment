7c80e9fb519f544b6f50bae09b16f67ba395cd04
rohandry <rohandrysdale95@gmail.com>
2022-08-24 13:44:28 +1000

    update scripts

--- buildscenario.sh
+++ buildscenario.sh
@@ -42,7 +42,7 @@ done
 ###### Debug a boken container
 echo "Creating broken Container"
 podman run -d --name mysql -e MYSQL_DATABASE=items -e MYSQL_USER=user1 -e MYSQL_PASSWRD=mypa55 -e MYSQL_ROOT_PASSWORD=r00tpa55 -p 30306:3306 docker.io/bitnami/mysql
-
+rm -Rf generatedFiles
 ####################################### PART B
 
 oc projects | grep test-project 
--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -Rf generatedFiles exerciseFiles/*
-echo 'podman run -d --name devsql -e MYSQL_DATABASE=items -e MYSQL_USER=user1 -e MYSQL_PASSWRD=mypa55 -e MYSQL_ROOT_PASSWORD=r00tpa55 -p 30306:3306 docker.io/bitnami/mysql' > generatedFiles/devSqlLaunch.sh
+echo 'podman run -d --name devsql -e MYSQL_DATABASE=items -e MYSQL_USER=user1 -e MYSQL_PASSWRD=mypa55 -e MYSQL_ROOT_PASSWORD=r00tpa55 -p 30306:3306 docker.io/bitnami/mysql' > providedFiles/devSqlLaunch.sh
 podman rm -f toDeleteTest1 toDeleteTest2 toDeleteTest3 toDeleteTest4 practiceContainer mysql yuriContainer
 podman rmi localhost/testimage1 localhost/testimage2 localhost/testimage3 localhost/testimage4
 oc delete project test-project 

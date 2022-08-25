3d0f77b09066c93248ef551d83509898de156cd0
rohandry <rohandrysdale95@gmail.com>
2022-08-25 11:26:04 +1000

    Modify Secrets

--- buildscenario.sh
+++ buildscenario.sh
@@ -46,26 +46,27 @@ podman run -d --name mysql -e MYSQL_DATABASE=items -e MYSQL_USER=user1 -e MYSQL_
 
 
 ####################################### PART B
-NOTE3='Dear Red Hatter, if you’re reading this its too late for me, they’ve probably told you I "disappeared". I havent managed to complete my work so its up to you to save Canberra.  Doesnt <b>K</b>osmos <b>G</b>alaxy <b>B</b>alistics sound familiar to you? Mark my words, they’re going to ask you to launch the "rocket" so Red Hat will be blamed for all of this. We have one last hope. I managed to create an alternative launch code for the countDown app that will trigger the missile to self-destruct. Please Red Hatter, save us all. The code is 190990. ~ Alan'
-DIARY1='The path is set. Soon our competition will be no more. Thank you Red Hat. Note to self, next diary is hidden in my Containerfile folder.'
+NOTE3='Dear Red Hatter, if youre reading this its too late for me, theyve probably told you I "disappeared". I havent managed to complete my work so its up to you to save Canberra.  Doesnt K-osmos G-alaxy B-alistics sound familiar to you? Mark my words, theyre going to ask you to launch the "rocket" so Red Hat will be blamed for all of this. We have one last hope. I managed to create an alternative launch code for the countDown app that will trigger the missile to self-destruct. Please Red Hatter, save us all. The code is 201199. ~ Alan'
+DIARY1='Diary 1: The path is set. Soon our competition will be no more. Thank you Red Hat. ~ Alan'
 
 #If the secret project doesnt exist create it
-oc projects | grep -q alans-secret-project
+USER=$(oc whoami)
+oc projects | grep -q ${USER}-alans-secret-project
 if [ $? == 1 ]
 then
-	oc new-project alans-secret-project
+	oc new-project ${USER}-alans-secret-project
         oc new-app --name mysecretdiary --context-dir providedFiles/hello-world-nginx --build-env WHICHINDEX="${DIARY1}" --strategy docker https://github.com/rohandry/myDO180Assignment
 	oc expose svc mysecretdiary
 fi
 
 #Project does not exist so create it
-oc projects | grep -q alans-test-project 
+oc projects | grep -q ${USER}-alans-test-project 
 if [ $? == 1 ]
 then
-	oc new-project alans-test-project 
+	oc new-project ${USER}-alans-test-project 
 else
 #Project exists so delete existing apps
-	oc project alans-test-project
+	oc project ${USER}-alans-test-project
         for APP in mytest buildapp htmlhelloworld yuricontainer containerbuild mariadbpersistent mytemplate
 	do
 		oc status | grep -q $APP
